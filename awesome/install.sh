fb0389705444562a0311953bcad14e85bafe02a4
root <root@localhost.localdomain>
2015-09-26 00:17:21 +0000

    primeiro commit

---
+++ awesome/install.sh
@@ -0,0 +1,8 @@
+
+pacman -Sy
+pacman -S xorg-server xorg-xinit xorg-server-utils xorg-twm awesome terminator vicious virtualbox-guest-modules virtualbox-guest-utils
+
+#hddtemp: for the HDD Temp widget type
+#alsa-utils: for the Volume widget type
+#wireless_tools: for the Wireless widget type
+#curl: for widget types accessing network resources [installed]
