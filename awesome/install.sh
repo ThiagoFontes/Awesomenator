e1c4f83ff929561333fbb81b9bd924f7d9152264
Thiago Fontes <thiago93br@gmail.com>
2015-09-25 22:02:38 -0300

    Adding conky and awesome wm configuration

--- awesome/install.sh
+++ awesome/install.sh
@@ -1,8 +1,13 @@
 
+#Don't use this if you don't know the packages!
+#
 pacman -Sy
-pacman -S xorg-server xorg-xinit xorg-server-utils xorg-twm awesome terminator vicious virtualbox-guest-modules virtualbox-guest-utils
+pacman -S xorg-server xorg-xinit xorg-server-utils xorg-twm awesome terminator vicious virtualbox-guest-modules virtualbox-guest-utils alsa-utils wireless_tools xcompmgr conky terminus-font
+
+#setting up conky
+cp .conkyrc ~/.conkyrc
 
 #hddtemp: for the HDD Temp widget type
 #alsa-utils: for the Volume widget type
 #wireless_tools: for the Wireless widget type
-#curl: for widget types accessing network resources [installed]
+#curl: for widget types accessing network resources
