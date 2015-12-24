836568f01833f9f5ae72498b490efd0c43addddd
Thiago Fontes <thiago93br@gmail.com>
2015-12-24 14:53:36 -0300

    Updating install script to be more user friendly

---
+++ install.sh
@@ -0,0 +1,16 @@
+
+#Don't use this if you don't know the packages!
+#
+pacman -Sy
+pacman -S xorg-server xorg-xinit xorg-server-utils xorg-twm awesome terminator vicious virtualbox-guest-modules virtualbox-guest-utils alsa-utils wireless_tools xcompmgr conky terminus-font
+
+#setting up conky
+cp .conkyrc ~/.conkyrc
+
+#setingup theme for current user
+cp awesome ~/.config/
+
+#hddtemp: for the HDD Temp widget type
+#alsa-utils: for the Volume widget type
+#wireless_tools: for the Wireless widget type
+#curl: for widget types accessing network resources
